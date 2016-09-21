68b6a05c44ed72d9b8e47da4d54d4fdde5edbe76
Jack Palmer <jack.palmer@bbc.co.uk>
2016-09-21 09:23:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub File Diff Ignore Whitespace Toggle
+
+Inject the javascript found in `insert.js` onto every **GitHub** page.
+
+![Screenshot](http://i.imgur.com/jBIcKsF.png)
+
+## Working pages
+* Pull Request File Diff
+* Pull Request Commit File Diff
---
+++ toggle.js
@@ -0,0 +1,24 @@
+javascript:(function () {
+
+  if(window.location.search = '') {
+    window.location.search += '?w=1';
+    return;
+  } else {
+    var params = window.location.search.substring(1, window.location.search.length).split('&');
+    var whitespaceOn = false;
+
+    for(var i = 0; i < params.length; i++) {
+      if(params[i] === 'w=1') {
+        params.splice(i, 1);
+        whitespaceOn = true;
+      }
+    }
+
+    if(!whitespaceOn) {
+      params.push('w=1');
+    }
+
+    window.location.search = params.join('&');
+    
+  }
+ })()
