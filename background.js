053c60ef587032d34780bebddc7e17b04643eeb0
Jack Palmer <jack.palmer@bbc.co.uk>
2016-09-22 17:26:54 +0100

    Beautifying files

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-// listen for our browerAction to be clicked
-
-chrome.tabs.onUpdated.addListener(function (tab, changeInfo) {
-	// for the current tab, inject the "inject.js" file & execute it
-  if (changeInfo.status == 'loading') {
-    chrome.tabs.executeScript(tab.ib, {
-  		file: 'inject.js'
-  	});
-  }
+chrome.tabs.onUpdated.addListener(function(tab, changeInfo) {
+    // for the current tab, inject the "inject.js" file & execute it
+    if (changeInfo.status == 'loading') {
+        chrome.tabs.executeScript(tab.ib, {
+            file: 'inject.js'
+        });
+    }
 });
--- inject.js
+++ inject.js
@@ -1,59 +1,57 @@
 (function() {
-  var pattern = /\/pull\/[0-9]+\/(files|commits\/[a-f0-9]+)|\/commit\/[a-f0-9]+/;
-  var markup_HTML = '<div class="diffbar-item"><button id="ignoreWhitespace" class="btn btn-sm btn-outline tooltipped tooltipped-s" type="button" aria-label="View diff with whitespace ignored">Ignore whitespace</button></div>';
+    var pattern = /\/pull\/[0-9]+\/(files|commits\/[a-f0-9]+)|\/commit\/[a-f0-9]+/;
+    var markup_HTML = '<div class="diffbar-item"><button id="ignoreWhitespace" class="btn btn-sm btn-outline tooltipped tooltipped-s" type="button" aria-label="View diff with whitespace ignored">Ignore whitespace</button></div>';
 
+    if (window.location.hostname === "github.com" &&
+        pattern.test(window.location.pathname) &&
+        document.getElementById('ignoreWhitespace') === null) {
 
-  if(window.location.hostname === "github.com"
-   && pattern.test(window.location.pathname)
-   && document.getElementById('ignoreWhitespace') === null) {
-    var container_DOM = document.querySelector('.float-right.pr-review-tools');
+        var container_DOM = document.querySelector('.float-right.pr-review-tools');
 
-    var InsertInDom = new Promise(
-      function(resolve, reject) {
-        container_DOM.insertAdjacentHTML('afterBegin', markup_HTML);
-        resolve();
-      }
-    );
-
-
-    InsertInDom.then(function(val) {
-      var WHITESPACE_PARAM = 'w=1';
-      var button_DOM = document.getElementById('ignoreWhitespace');
-
-      if(window.location.search.indexOf(WHITESPACE_PARAM) > -1) {
-        button_DOM.classList.add('selected');
-      }
-
-      button_DOM.addEventListener('click', function () {
-        if(window.location.search = '') {
-          window.location.search += '?' + WHITESPACE_PARAM;
-          return;
-        } else {
-          var params = window.location.search.substring(1, window.location.search.length).split('&');
-          var whitespaceOn = false;
-
-          for(var i = 0; i < params.length; i++) {
-            if(params[i] === WHITESPACE_PARAM) {
-              params.splice(i, 1);
-              whitespaceOn = true;
+        var InsertInDom = new Promise(
+            function(resolve, reject) {
+                container_DOM.insertAdjacentHTML('afterBegin', markup_HTML);
+                resolve();
             }
-          }
+        );
 
-          if(!whitespaceOn) {
-            params.push(WHITESPACE_PARAM);
-          }
+        InsertInDom.then(function(val) {
+                var WHITESPACE_PARAM = 'w=1';
+                var button_DOM = document.getElementById('ignoreWhitespace');
 
-          window.location.search = params.join('&');
+                if (window.location.search.indexOf(WHITESPACE_PARAM) > -1) {
+                    button_DOM.classList.add('selected');
+                }
 
-        }
-        document.getElementById('ignoreWhitespace').classList.toggle('selected');
+                button_DOM.addEventListener('click', function() {
+                    if (window.location.search = '') {
+                        window.location.search += '?' + WHITESPACE_PARAM;
+                        return;
+                    } else {
+                        var params = window.location.search.substring(1, window.location.search.length).split('&');
+                        var whitespaceOn = false;
 
-      });
-    })
-    .catch(function(reason) {
-      console.warn('Unable to inject whitespace button: ' + reason);
-    });
+                        for (var i = 0; i < params.length; i++) {
+                            if (params[i] === WHITESPACE_PARAM) {
+                                params.splice(i, 1);
+                                whitespaceOn = true;
+                            }
+                        }
 
-  }
+                        if (!whitespaceOn) {
+                            params.push(WHITESPACE_PARAM);
+                        }
+
+                        window.location.search = params.join('&');
+
+                    }
+                    document.getElementById('ignoreWhitespace').classList.toggle('selected');
+
+                });
+            })
+            .catch(function(reason) {
+                console.warn('Unable to inject whitespace button: ' + reason);
+            });
+    }
 
 })()
